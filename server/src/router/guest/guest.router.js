117ad62b9acf3966d31ef37e8e4c4c1360c99716
alex <diefaust1993@gmail.com>
2025-08-24 14:29:34 +0400

    Fix app config router

--- server/src/router/guest/guest.router.js
+++ server/src/router/guest/guest.router.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import authRouter from './auth.router.js';
 import inventoryRouter from './inventory.router.js';
+import appRouter from "./app/app.router.js";
 
 const router = express.Router();
 router.use('/auth', authRouter);
 router.use('/inventory', inventoryRouter);
+router.use('/app', appRouter);
 
 export default router;
